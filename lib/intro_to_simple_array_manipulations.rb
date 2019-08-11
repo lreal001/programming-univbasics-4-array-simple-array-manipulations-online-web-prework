4223d1a1b6a3e8292764c27f1b0cb16517fd4483
Lessllie Real <rlessllie@yahoo.com>
2019-08-11 18:47:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -34,7 +34,6 @@ end
 
 def using_insert
   @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-    @another_language = "Python" = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
     @another_language = "Python"
     @list_of_programming_languages.push("Python")
 end
